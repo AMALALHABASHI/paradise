0d77935a657681b2e244a6b0e1dc2ee5f4b29d4b
AMALALHABASHI <aml.alhbshi@gmail.com>
2021-12-31 02:05:05 +0300

    first commit

---
+++ database/migrations/2021_12_28_235015_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('product_id');
+            $table->timestamps();
+            $table->string('product_name');
+            $table->text('description')->nullable();
+            $table->double('price');
+            $table->boolean('is_sale')->default(0);
+            $table->double('sales')->nullable();
+            $table->double('size');
+            $table->string('color');
+            $table->double('sales');
+            $table->string('img')->nullable();
+            $table->BigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
